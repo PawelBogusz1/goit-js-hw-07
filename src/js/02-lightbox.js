28afbdca69b7f7bfe5c4e8e605e678a8f932532e
Pawel Bogusz1 <pbog2000@gmail.com>
2023-02-19 00:36:50 +0100

    js-07

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galaryElem = galleryItems
+    .map(
+        (item) =>
+        
+           `<li><a class="gallery__link" href=${item.original}>
+            <img 
+            class="gallery__image" src=${item.preview} 
+            data-source=${item.original} 
+            alt=${item.description}/>
+            </a></li>`        
+    )
+    .join("");
+    
+    gallery.innerHTML = galaryElem;
+
+  
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+ 
+
+  });
